024db4b12bbc01078993f6493be6a2f876d6c780
David Do <55934975+D-vidDo@users.noreply.github.com>
2025-08-05 20:06:19 -0600

    Revert "Update firebase.ts"
    This reverts commit f950fb5719bbb30291c509d0a50f5602ba90907f.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCFXQuWiV6sKVj7XbWQ7WZ-zd9lttL2Jkw",
-  authDomain: "ncl-league.firebaseapp.com",
-  projectId: "ncl-league",
-  storageBucket: "ncl-league.firebasestorage.app",
-  messagingSenderId: "943379047142",
-  appId: "1:943379047142:web:a34cab796d01d620ae5127",
-  measurementId: "G-FJET4TYGD9"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  // ...other config from Firebase Console
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
