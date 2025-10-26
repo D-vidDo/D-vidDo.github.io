c876a951fd559c1866a91afa4fbefe78b3fb41f1
David Do <55934975+D-vidDo@users.noreply.github.com>
2025-10-26 15:48:35 -0600

    Update Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -456,12 +456,13 @@ const AdminGameEntry = () => {
                       onChange={() => toggleSubPlayer(player.id)}
                       className="h-4 w-4 rounded border-muted text-primary focus:ring-primary/60"
                     />
-                    <span
-  className={`truncate ${
-    standIns[player.id] ? "line-through text-gray-400" : ""
-  }`}
->
-  {player.name} {standIns[player.id] ? `(${allPlayers.find(p => p.id === standIns[player.id])?.name})` : ""}
+                    <span className="truncate">
+  <span className={standIns[player.id] ? "line-through text-gray-400" : ""}>
+    {player.name}
+  </span>
+  {standIns[player.id] && (
+    <> ({allPlayers.find((p) => p.id === standIns[player.id])?.name})</>
+  )}
 </span>
 
                   </label>
