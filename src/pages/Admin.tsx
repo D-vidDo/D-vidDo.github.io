d0bf9b40bbd5530cf3d1799e72773ec2bf35e6cc
David Do <55934975+D-vidDo@users.noreply.github.com>
2025-11-06 18:51:02 -0700

    altered games to remove after 1 week from admin

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -104,7 +104,7 @@ const AdminGameEntry = () => {
 
       const now = new Date();
       const threeDaysAgo = new Date();
-      threeDaysAgo.setDate(now.getDate() - 3);
+      threeDaysAgo.setDate(now.getDate() - 7);
 
       const filteredGames = data.filter((g) => {
         if (!g.date) return false;
