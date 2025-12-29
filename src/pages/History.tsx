590416967cf67e50ea0db13c540ac81d706c5cad
David Do <55934975+D-vidDo@users.noreply.github.com>
2025-12-28 20:59:23 -0700

    Update History.tsx

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -266,7 +266,7 @@ export default function History({ seasonId }: { seasonId: number }) {
                       <tr
                         key={`${game.id}-set-${set.set_no}`}
                         style={{
-                          background: `linear-gradient(90deg, #ffffff
+                          background: `linear-gradient(270deg, #ffffff
              85%, ${getTeamColors(game.team_id).bg || "#ffffff"} 100%)`,
                         }}
                         className="border-b border-muted"
