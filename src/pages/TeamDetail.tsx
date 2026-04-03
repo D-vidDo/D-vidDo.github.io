6db351cb88c190558b7a245beae238792c886380
David Do <55934975+D-vidDo@users.noreply.github.com>
2026-04-03 02:15:58 -0600

    Update TeamDetail.tsx

--- src/pages/TeamDetail.tsx
+++ src/pages/TeamDetail.tsx
@@ -162,7 +162,7 @@ useEffect(() => {
             name
           )
         `)
-        .in("trade_id", [4,5])
+        .in("trade_id", [4,5,6])
         .or(`to_team.eq.${teamData.name},from_team.eq.${teamData.name}`)
         .order("created_at", { ascending: false });
 
