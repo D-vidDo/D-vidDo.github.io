ebdc913d9fada9ba29bab905ecf69d4ea6ee298c
David Do <david-do1@outlook.com>
2026-08-06 14:26:04 -0600

    NCL SUMMER WEEK 5

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -281,9 +281,9 @@ const Home = () => {
 
         {/* <UpcomingGames /> */}
 
-<MatchupCard matchupId={82}/>
-<MatchupCard matchupId={83}/>
-<MatchupCard matchupId={84}/>
+<MatchupCard matchupId={85}/>
+<MatchupCard matchupId={86}/>
+<MatchupCard matchupId={87}/>
 
 
 
